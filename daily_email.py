f848deafe5c38e7ac7eaeb2c8619f199496df9bc
LovedayBrooke <katmatfield@gmail.com>
2013-06-13 21:35:51 +0100

    Pep8ify daily_email.py

--- daily_email.py
+++ daily_email.py
@@ -1,7 +1,10 @@
 import os
 import sys
-for k in [k for k in sys.modules if k.startswith('django')]: 
-    del sys.modules[k] 
+import datetime
+import logging
+
+for k in [k for k in sys.modules if k.startswith('django')]:
+    del sys.modules[k]
 from google.appengine.dist import use_library
 use_library('django', '1.2')
 from google.appengine.ext import db
@@ -9,33 +12,43 @@ from google.appengine.ext.webapp import template
 from google.appengine.ext import webapp
 from google.appengine.ext.webapp.util import run_wsgi_app
 from google.appengine.api import mail
-import datetime
-import pocketaccountant
+
+from pocketaccountant import *
 import emailreceiver
-import logging
 import traceback
+
 
 class DailyEmail(webapp.RequestHandler):
     def get(self):
         try:
             today = datetime.datetime.today()
-            yesterday_6am = datetime.datetime(today.year, today.month, today.day, 6, 0) - datetime.timedelta(days=1)
-            week_start = datetime.datetime(today.year, today.month, today.day, 6, 0) - datetime.timedelta(days=datetime.datetime.weekday(today))
+            yesterday_6am = datetime.datetime(today.year, today.month,
+                today.day, 6, 0) - datetime.timedelta(days=1)
+            week_start = datetime.datetime(today.year, today.month, today.day,
+                6, 0) - datetime.timedelta(
+                days=datetime.datetime.weekday(today))
             month_start = datetime.datetime(today.year, today.month, 1, 6, 0)
-            
-            spending_breakdown = pocketaccountant.Logged_spending.spending_during_period(yesterday_6am)[0]
-            yesterday_total = pocketaccountant.Logged_spending.spending_during_period(yesterday_6am)[1]
-            week_total = pocketaccountant.Logged_spending.spending_during_period(week_start)[1]
-            month_total = pocketaccountant.Logged_spending.spending_during_period(month_start)[1]
-            
+
+            spending_breakdown = Logged_spending.spending_during_period(
+                yesterday_6am)[0]
+            yesterday_total = Logged_spending.spending_during_period(
+                yesterday_6am)[1]
+            week_total = Logged_spending.spending_during_period(week_start)[1]
+            month_total = Logged_spending.spending_during_period(
+                month_start)[1]
+
             message = mail.EmailMessage()
-            message.sender = "PocketAccountant@pocketaccountant.appspotmail.com"
+            message.sender = ("PocketAccountant@"
+                "pocketaccountant.appspotmail.com")
             message.subject = "Yesterday's spending"
             message.to = emailreceiver.address
-            message.body = "Yesterday's spending: " + yesterday_total + "\nWeek's spending so far: " + week_total + "\nMonth's spending so far: " + month_total + "\n\nBreakdown:\n" + spending_breakdown
-            
+            message.body = ("Yesterday's spending: {0}\n"
+                "Week's spending so far: {1}\n"
+                "Month's spending so far: {2}\n\n"
+                "Breakdown:{3}\n").format(yesterday_total, week_total,
+                month_total, spending_breakdown)
             message.send()
-        
+
         except Exception:
             stacktrace = traceback.format_exc()
             logging.error("%s", stacktrace)
