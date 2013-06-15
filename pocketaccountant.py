86b7253b73bedc39f125ff2bd8ff0109cbdcc2c0
LovedayBrooke <katmatfield@gmail.com>
2013-06-15 16:18:35 +0100

    Move all the code to one file and tidy it up

--- pocketaccountant.py
+++ pocketaccountant.py
@@ -9,56 +9,192 @@ import simplejson as json
 import secrets
 import emailreceiver
 
-class Logged_spending(db.Model):
+
+class LoggedSpending(db.Model):
     descrip = db.StringProperty()
     amount = db.IntegerProperty()
     date = db.DateTimeProperty()
 
     @classmethod
-    def spending_during_period(self, start):
-        end = datetime.datetime(datetime.date.today().year,
-            datetime.date.today().month, datetime.date.today().day, 6)
-        total = 0
+    def create(cls, dm_text, dm_date):
+        spending = LoggedSpending()
+        # there are two possible formats for this:
+        # 1) 1.20, fruit on monday
+        # 2) 1.20,fruit on monday
+        # 3) 1.20 fruit on monday
+        # Detect which of these it is, and split accordingly to end up with
+        # a list where the first item is the amount, the second the descrip
+        if ', ' in dm_text:
+            dm_split = dm_text.split(', ')
+        elif ',' in dm_text:
+            dm_split = dm_text.split(',')
+        else:
+            entire_split = dm_text.split(' ')
+            dm_split = [entire_split[0],
+                ' '.join(entire_split[1:])]
+
+        spending.descrip = dm_split[1]
+        spending.amount = int("".join(dm_split[0].split('.')))
+        spending.date = dm_date
+        db.put(spending)
+
+    @classmethod
+    def itemised_spending_in_period(self, start):
+        """ Put together a string listing all the transaction details
+        between the start date and today, 6am
+        """
+
+        today = datetime.date.today()
+        end = datetime.datetime(today.year, today.month, today.day, 6)
+
+        any_transactions = False
+
         spending_breakdown = ''
-        spendings = Logged_spending.all().filter('date >', start).filter(
+        spendings = LoggedSpending.all().filter('date >', start).filter(
             'date <', end)
         for spending in spendings:
             spending_breakdown += "    {0} on {1}\n".format(
-                Logged_spending.convert_money_to_string(spending.amount),
+                cls.convert_money_to_string(spending.amount),
                 spending.descrip)
+            any_transactions = True
+
+        if not any_transactions:
+            spending_breakdown = "No spending"
+
+        return spending_breakdown
+
+    @classmethod
+    def total_spending_in_period(cls, start):
+        """ Return a total for all the spending since start to today 6am """
+
+        today = datetime.datetime.today()
+        end = datetime.datetime(today.year, today.month, today.day, 6)
+
+        total = 0
+        spendings = LoggedSpending.all().filter('date >', start).filter(
+            'date <', end)
+        for spending in spendings:
             total += spending.amount
-        if total == 0:
-            spending_breakdown = "no spending"
-        return spending_breakdown, Logged_spending.convert_money_to_string(
-            total)
+
+        return LoggedSpending.convert_money_to_string(total)
 
     @staticmethod
-    def convert_money_to_string(amount):
-        if amount == 0:
+    def convert_money_to_string(cls, amount):
+        """ Convert the amount (stored in pence) into a string like £2.50 """
+
+        if self.amount == 0:
             pounds = '0'
             pence = '00'
         else:
-            amount = str(amount)
-            pence = amount[-2:]
+            self.amount = str(self.amount)
+            pence = self.amount[-2:]
+            # check if any pound figures have been given. If not, insert a 0.
             if len(amount) > 2:
-                pounds = amount[:(len(amount)-2)]
+                pounds =self.amount[:(len(self.amount)-2)]
             else:
                 pounds = '0'
+
         return u"\xA3" + pounds + "." + pence
 
 
-def correct_for_dst(today):
-    spring2011 = datetime.datetime(2011, 03, 27, 1, 0)
-    autumn2011 = datetime.datetime(2011, 10, 30, 2, 0)
-    spring2012 = datetime.datetime(2012, 03, 25, 1, 0)
-    autumn2011 = datetime.datetime(2012, 10, 28, 2, 0)
-    spring2013 = datetime.datetime(2013, 03, 31, 1, 0)
-    autumn2013 = datetime.datetime(2013, 10, 27, 2, 0)
-    spring2014 = datetime.datetime(2014, 03, 30, 1, 0)
-    autumn2014 = datetime.datetime(2014, 10, 26, 2, 0)
-    spring2015 = datetime.datetime(2015, 03, 29, 1, 0)
-    autumn2015 = datetime.datetime(2015, 10, 25, 2, 0)
-    if today >= spring2011 and today <= autumn2011:
-        return today + datetime.timedelta(0, 3600)
-    else:
-        return today
+class DirectMessage(db.Model):
+    text = db.StringProperty()
+    id = db.StringProperty()
+    date = db.DateTimeProperty()
+
+    @classmethod
+    def last_DM_ID(self):
+        last_DM = DirectMessage.all().order('-date').get()
+        return last_DM.id
+
+    @classmethod
+    def create(cls, dm_json):
+        newDM = DirectMessage()
+        newDM.id = dm_json['id_str']
+        newDM.text = dm_json['text']
+        newDM.date = DirectMessage.make_datetime(dm_content['created_at'])
+        db.put(newDM)
+        return newDM
+
+    @staticmethod
+    def make_datetime(date_string):
+        """ Convert the date string from the twitter json (eg "Fri Jun 14 
+        20:52:08 +0000 2013" to a datetime
+        """
+
+        return datetime.datetime.strptime(string.join(date_string.split(
+            ' +0000 ')), '%a %b %d %H:%M:%S %Y')
+
+
+class TwitterPull(webapp2.RequestHandler):
+    def get(self):
+        """ Get the latest DMs from the Twitter API, make them into dm and 
+        logged spending objects
+        """
+
+        dm_json = cls.get_dm_json()
+        if not dm_json:
+            logging.info("No new DMs")
+        for dm in dm_json:
+            dm_obj = DirectMessage.create(dm)
+            LoggedSpending.create(dm['text'], dm_obj.date)
+
+    @classmethod
+    def get_dm_json(cls):
+        """ Request the most recent DMs since the last pull from the Twitter 
+        API, and return the response as json
+        """
+
+        consumer = oauth.Consumer(key='A0YdjSUGSwKPfEEF1ThQ',
+            secret=secrets.consumer)
+        token = oauth.Token(
+            key='371653560-dLklDiFqg8hMKOsskiF0MDmdCLOrwKhwH08vyq0E',
+            secret=secrets.access)
+        client = oauth.Client(consumer, token)
+        url = ('http://api.twitter.com/1.1/direct_messages'
+            '.json?since_id='+DirectMessage.last_DM_ID())
+        resp, content = client.request(url,
+            method="GET",
+            body=None,
+            headers=None,
+            force_auth_header=True)
+        return json.loads(content)
+
+
+class DailyEmail(webapp2.RequestHandler):
+    def get(self):
+        """ Get all the information needed for the daily email, make & send
+        the email
+        """
+        
+        today = datetime.datetime.today()
+        today_6am = datetime.datetime(today.year, today.month, today.day, 6, 0)
+
+        yesterday_6am =  today_6am - datetime.timedelta(days=1)
+        breakdown = Logged_spending.itemised_spending_in_period(
+            yesterday_6am)
+        yesterday_total = Logged_spending.total_spending_in_period(
+            yesterday_6am)
+
+        week_start =  today_6am - datetime.timedelta(
+            days=datetime.datetime.weekday(today))
+        week_total = Logged_spending.total_spending_in_period(week_start)
+
+        month_start = datetime.datetime(today.year, today.month, 1, 6, 0)
+        month_total = Logged_spending.total_spending_in_period(month_start)
+
+        message = mail.EmailMessage()
+        message.sender = ("PocketAccountant@"
+            "pocketaccountant.appspotmail.com")
+        message.subject = "Yesterday's spending: {0}".format(yesterday_total)
+        message.to = emailreceiver.address
+        message.body = ("Yesterday's spending: {0}\n"
+            "Week's spending so far: {1}\n"
+            "Month's spending so far: {2}\n\n"
+            "Breakdown: {3}\n").format(yesterday_total, week_total,
+            month_total, breakdown)
+        message.send()
+
+application = webapp2.WSGIApplication([('/daily_email', DailyEmail),
+                                      ('/twitterpull', TwitterPull)],
+                                     debug=True)
